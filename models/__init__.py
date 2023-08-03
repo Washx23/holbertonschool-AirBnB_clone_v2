c28ab93f874ebbf768234277db162187b64d4d17
Washx23 <5731@holbertonstudents.com>
2023-08-03 11:45:32 -0700

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+    This module instantiates the storage object to persist
+    objects (either to a file or a database)
+"""
+from os import getenv
 
+storageType = getenv("HBNB_TYPE_STORAGE")
 
-storage = FileStorage()
-storage.reload()
+if storageType == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from os import getenv
+# from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class for storing state information"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
