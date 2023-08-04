e267a88377fb192558a9ef8c51ccf4c67db109c7
Washx23 <5731@holbertonstudents.com>
2023-08-04 09:43:34 -0700

    ....

--- models/state.py
+++ models/state.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-import models
+from models import storage
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    # Relationship configuration for DBStorage
-    if models.storage_type == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
 
-    # Getter attribute for FileStorage
-    if models.storage_type == 'file':
+    else:
         @property
         def cities(self):
-            """ Getter attribute that returns the list of City instances """
-            from models import storage
-            city_list = []
-            for city in storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """ getter attribute for cities """
+            from models.city import City
+
+            objects = storage.all(City)
+            return [obj for obj in objects.values() if obj.state_id == self.id]
