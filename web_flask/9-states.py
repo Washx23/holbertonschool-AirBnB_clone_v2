1288f96f8c5677f0edb1be490dd3bfefddbefedc
Washx23 <5731@holbertonstudents.com>
2023-08-17 14:24:38 -0700

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-"""
-    This module starts a Flask Web App
-    that interacts with our AirBnB engine
-"""
+"""Script that starts a web application"""
 
-
+from models import storage
+from models.state import State
 from flask import Flask, render_template
-from models import storage, State, City
 
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def display_states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('states.html', states=sorted_states)
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def display_state_cities(id):
-    state = storage.get(State, id)
-    if state:
-        cities = sorted(state.cities, key=lambda city: city.name)
-        return render_template('state.html', state=state, cities=cities)
-    else:
-        return render_template('not_found.html')
+def state_id(id=None):
+    states = storage.all(State).values()
+    st = None
+    for state in states:
+        if id == state.id:
+            st = state
+            break
+    return render_template('9-states.html', states=states, id=id, st=st)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def closed(exit):
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
