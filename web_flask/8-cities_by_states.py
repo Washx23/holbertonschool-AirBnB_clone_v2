5c01f5365979cff71e24d40cc0b5c7b3387c4e62
Washx23 <5731@holbertonstudents.com>
2023-08-17 14:11:47 -0700

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,30 +3,25 @@
     This module starts a Flask Web App
     that interacts with our AirBnB engine
 """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def states_list():
-    """
-        Displays the list of all states and their respective cities
-        in alphabetical order in an HTML page
-    """
-    states = storage.all(State)
-    stateList = [state for state in states.values()]
-    srtStates = sorted(stateList, key=lambda s: s.name)
-    return render_template("8-cities_by_states.html", states=srtStates)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a list of all State objects and their linked City objects"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Close the DB session """
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
