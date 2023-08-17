0f0a8c4c804658027b20c28020afed3ebf678816
Washx23 <5731@holbertonstudents.com>
2023-08-17 13:58:48 -0700

    fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,48 +1,58 @@
 #!/usr/bin/python3
+
+
 """
     This module creates a web app with Flask
 """
+
+
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return f"C {escape(text.replace('_', ' '))}"
+    text = text.replace('_', ' ')
+    return f"C {text}"
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def py_text(text="is cool"):
-    return f"Python {escape(text.replace('_', ' '))}"
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_text(text="is cool"):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def display_n(n):
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
     return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template("5-number.html", num=n)
+    return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template("6-number_odd_or_even.html", num=n)
+    if n % 2 == 0:
+        odd_or_even = "even"
+    else:
+        odd_or_even = "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
